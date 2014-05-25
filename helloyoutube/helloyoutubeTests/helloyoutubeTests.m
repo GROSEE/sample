3482da64972c2cde2916baf72914985382424af1
GROSEE <grosee.campany@gmail.com>
2014-05-25 21:09:24 +0900

    no message

--- helloyoutube/helloyoutubeTests/helloyoutubeTests.m
+++ helloyoutube/helloyoutubeTests/helloyoutubeTests.m
@@ -2,7 +2,7 @@
 //  helloyoutubeTests.m
 //  helloyoutubeTests
 //
-//  Created by Fumiya Ogawa on 2014/05/25.
+//  Created by GROSEE on 2014/05/25.
 //  Copyright (c) 2014年 grosee. All rights reserved.
 //
 
