00abdbf5aeeb01eca579fa244a133fb4613e733a
GROSEE <grosee.campany@gmail.com>
2014-05-25 21:08:20 +0900

    「helloyoutube」のコード

---
+++ helloyoutube/helloyoutube/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  helloyoutube
+//
+//  Created by Fumiya Ogawa on 2014/05/25.
+//  Copyright (c) 2014年 grosee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ helloyoutube/helloyoutubeTests/helloyoutubeTests.m
@@ -0,0 +1,34 @@
+//
+//  helloyoutubeTests.m
+//  helloyoutubeTests
+//
+//  Created by Fumiya Ogawa on 2014/05/25.
+//  Copyright (c) 2014年 grosee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface helloyoutubeTests : XCTestCase
+
+@end
+
+@implementation helloyoutubeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
